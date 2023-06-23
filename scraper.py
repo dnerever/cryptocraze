87f1ff07376af31a244260d205eed91348774b23
Keith Bates <bateskeith@pm.me>
2023-06-23 16:58:35 -0600

    Working on accessing specific JSON data

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import json
 url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
     'start':'1',
-    'limit':'5000',
+    'limit':'10',
     'convert':'USD'
 }
 headers = {
@@ -19,7 +19,13 @@ session.headers.update(headers)
 
 try:
     response = session.get(url, params=parameters)
-    data =json.loads(response.text)
-    print(data)
+    rawData = json.loads(response.text)
+    rawStatus = json.loads(response)
+    print(rawData['status'])
+    try:
+        with open('output.json', 'w', encoding='utf-8') as output:
+            json.dump(rawData, output, ensure_ascii=False, indent=4)
+    except:
+        print("\n\n\nFailed to save to File\n\n\n")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
