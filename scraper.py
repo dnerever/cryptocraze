61478ef20c684aef43b7795a8ea1f042b60e9471
Keith Bates <bateskeith@pm.me>
2023-07-06 23:44:16 -0600

    Using pandas to create a dataframe for working with the json

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import config   #import API Key
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+import pandas
 
 url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
@@ -21,13 +22,21 @@ session.headers.update(headers)
 
 try:
     response = session.get(url, params=parameters)
-    rawData = json.loads(response.text)
-    # rawStatus = json.loads(response)
-    # print(rawData['status'])
+    jsonData = json.loads(response.text)
+    print('Data Scraped')
     try:
         with open('output.json', 'w', encoding='utf-8') as output:
-            json.dump(rawData, output, ensure_ascii=False, indent=4)
+            json.dump(jsonData, output, ensure_ascii=False, indent=4)
+        print('JSON data saved to output.json')
     except:
-        print("\n\n\nFailed to save to File\n\n\n")
+        print("**Failed to save to File**")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
+
+try:
+    df = pandas.DataFrame.from_dict(pandas.json_normalize(jsonData), orient='columns')
+    df.to_csv('output.csv', encoding='utf-8', index=False)
+    df.to_json('pandasOutput.json')
+    print('Data saved to pandas')
+except:
+    print('**Failed to load to pandas**')
