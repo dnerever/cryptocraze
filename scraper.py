077d51c9bbda8df9db3758d600ab609a69bbd8ab
Keith Bates <bateskeith@pm.me>
2023-07-07 22:49:01 -0600

    Messing around with pandas.read_clipboard()

--- scraper.py
+++ scraper.py
@@ -23,11 +23,11 @@ session.headers.update(headers)
 try:
     response = session.get(url, params=parameters)
     jsonData = json.loads(response.text)
-    print('Data Scraped')
+    print('* Data Scraped')
     try:
         with open('output.json', 'w', encoding='utf-8') as output:
             json.dump(jsonData, output, ensure_ascii=False, indent=4)
-        print('JSON data saved to output.json')
+        print('* data saved to output.json')
     except:
         print("**Failed to save to File**")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
@@ -37,6 +37,21 @@ try:
     df = pandas.DataFrame.from_dict(pandas.json_normalize(jsonData), orient='columns')
     df.to_csv('output.csv', encoding='utf-8', index=False)
     df.to_json('pandasOutput.json')
-    print('Data saved to pandas')
+    print('* Data saved to pandas')
 except:
     print('**Failed to load to pandas**')
+
+try:
+    dataID = pandas.read_clipboard(
+        sep=",",
+        header="infer",
+        index_col=0,
+        names=["Order", "ID", "Category", "Sales", "Quantity", "Discount"],
+    )
+    pandas.set_option("display.max_rows", None, "display.max_columns", None)
+    print(dataID)
+    # print(dataID)
+    dataID.to_csv('clipboardData.csv')
+    # print('* Data: ')
+except:
+    print('**Failed to print panda data**')
